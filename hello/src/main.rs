d037c467732e993f9a8f5292f078579c4451ef66
Daigo Hirooka <daigo.hirooka@gmail.com>
2022-04-23 18:50:02 +0900

    add ignore settings

---
+++ hello/src/main.rs
@@ -0,0 +1,11 @@
+use regex::Regex;
+use std::collections::HashMap;
+use std::io::Write;
+
+fn main() {
+    // let regex = Regex::new("foo.*").unwrap();
+    println!("Hello, world!");
+    std::thread::sleep_ms(100);
+
+    std::thread::sleep_ms(100);
+}
